60afc43514b4bffc7223944332c8f6791dcd5c63
tjuanluis04-bit <tjuanluis04@gmail.com>
2026-07-06 20:49:43 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'glosa-v1';
+const CACHE_NAME = 'glosa-v2';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
